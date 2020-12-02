722e45d423f27ee0c02b81f650e781a4833d6be0
Matt <matt.bowa@mail.com>
2020-12-02 02:49:42 +0000

    Initial commit

---
+++ client/apollo.config.js
@@ -0,0 +1,16 @@
+/*
+module.exports = {
+  client: {
+    name: 'Space Explorer [web]',
+    service: 'space-explorer',
+  },
+};
+*/
+module.exports = {
+  client: {
+    name: 'Space Explorer [web]',
+    service: 'My Graph',
+  },
+};
+
+
